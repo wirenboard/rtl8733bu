e40e5be3acc824f3e94aaa0d9a132cc334e511ec
Anton Tarasov <anton.tarasov@wirenboard.com>
2024-10-01 09:02:49 +0300

    Fix building with kernel 5.10

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -356,7 +356,12 @@ struct rtw_usb_drv usb_drv = {
 	.usbdrv.reset_resume   = rtw_resume,
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 8, 0)
+    .usbdrv.drvwrap.driver.shutdown = rtw_dev_shutdown,
+#else
 	.usbdrv.driver.shutdown = rtw_dev_shutdown,
+#endif
+
 };
 
 static inline int RT_usb_endpoint_dir_in(const struct usb_endpoint_descriptor *epd)
